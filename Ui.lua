033430d0fcc6a6c0a2cda1eab21d900ec3e93ac8
x-rodev <jacobfoster748@gmail.com>
2025-11-30 21:27:41 -0800

    Update Ui.lua

--- Ui.lua
+++ Ui.lua
@@ -1002,7 +1002,7 @@ end)
 
         local function UpdatePageSize()
             local correction = pageLayout.AbsoluteContentSize
-            page.CanvasSize = UDim2.new(0, correction.X+13, 0, correction.Y+500)
+            page.CanvasSize = UDim2.new(0, correction.X+13, 0, correction.Y+2000)
         end
 
         page.ChildAdded:Connect(UpdatePageSize)
